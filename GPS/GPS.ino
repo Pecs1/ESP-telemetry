1afd6e776132fc03e60f8b6a4a8582d5ed3d97a1
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-07-02 14:33:12 +0200

    feat: persistant storage will be later on implemeted to each board
    havent tested yet, but should work™

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,9 +1,41 @@
 #include "gps.env.h"
 
+// for wifi communication
 #include <esp_now.h>
 #include <WiFi.h>
 
+// persistant storage between reboots
+#include <Preferences.h>
+
+// esp_now.h
+esp_now_peer_info_t peerInfo;
+
+// preferences.h
+Preferences prefs;
+
+
 void setup() {
+     Serial.begin(115200);
+
+     // docs: https://docs.espressif.com/projects/arduino-esp32/en/latest/tutorials/preferences.html
+     // read-only the persistant storage
+     prefs.begin("sys_state", RO);
+     
+     // check if persistant storage has been created
+     if (prefs.isKey("ready") == false) {
+          prefs.end(); // close RO
+          prefs.begin("sys_state", RW); // reopen in RW
+
+          // write to persistant storage
+          prefs.putBool("ready", true);
+          prefs.putBool("is_normal", true);
+
+          prefs.end(); // close RW
+          prefs.begin("sys_state", RO); // reopen in RO
+     }
+
+     isNormal = prefs.getBool("is_normal");
+     prefs.end(); // close RO
 
 }
 
--- GPS/gps.env.example.h
+++ GPS/gps.env.example.h
@@ -1,4 +1,16 @@
 #ifndef GPS_ENV_H
 #define GPS_ENV_H
 
+// --- main configs ---
+const uint16_t MAINTENANCE_PIN = 0000; // set your 4 digit pin, must be the same between boards!
+
+// --- main board MAC ---
+uint8_t mainAddress[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+
+// --- aliases ---
+// for Preferences
+#define RW false // read-write
+#define RO true  // read-only
+
 #endif
