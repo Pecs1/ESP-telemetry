46f669dfde182f5dfd9d47a6df579dfc2bc87833
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-07-03 22:06:24 +0200

    huh didnt know arduino-ide had a linter the more you know

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,12 +1,16 @@
 #include "gps.env.h"
 
-// for wifi communication
+// for normal mode
 #include <esp_now.h>
 #include <WiFi.h>
+
+// for maintenance mode
 #include <WiFiAP.h>
+
 
 // persistant storage between reboots
 #include <Preferences.h>
+
 
 // esp_now.h
 esp_now_peer_info_t peerInfo;
@@ -16,94 +20,106 @@ Preferences prefs;
 
 
 void setup() {
-     Serial.begin(115200);
+  Serial.begin(115200);
 
-     // docs: https://docs.espressif.com/projects/arduino-esp32/en/latest/tutorials/preferences.html
-     // read-only the persistant storage
-     prefs.begin("sys_state", RO);
-     
-     // check if persistant storage has been created
-     if (prefs.isKey("ready") == false) {
-          prefs.end(); // close RO
-          prefs.begin("sys_state", RW); // reopen in RW
+  // docs: https://docs.espressif.com/projects/arduino-esp32/en/latest/tutorials/preferences.html
+  // read-only the persistant storage
+  prefs.begin("sys_state", RO);
 
-          // write to persistant storage
-          prefs.putBool("ready", true);
-          prefs.putUChar("current_mode", 0);
+  // check if persistant storage has been created
+  if (prefs.isKey("ready") == false) {
+    prefs.end();                   // close RO
+    prefs.begin("sys_state", RW);  // reopen in RW
 
-          prefs.end(); // close RW
-          prefs.begin("sys_state", RO); // reopen in RO
-     }
+    // write to persistant storage
+    prefs.putBool("ready", true);
+    prefs.putUChar("current_mode", 0);
 
-     uint8_t savedMode = prefs.getUChar("current_mode");
-     currentMode = (SystemMode)savedMode;
+    prefs.end();                   // close RW
+    prefs.begin("sys_state", RO);  // reopen in RO
+  }
 
-     prefs.end(); // close RO
+  uint8_t savedMode = prefs.getUChar("current_mode");
+  currentMode = (SystemMode)savedMode;
+
+  prefs.end();  // close RO
 
 
-     switch (currentMode) {
-       case NORMAL:
-          initNormal();
-          break;
+  switch (currentMode) {
+    case NORMAL:
+      initNormal();
+      break;
 
-       case MAINTENANCE:
-          initMaintenance();
-          break;
-          
-       default:
-          initFailSafe();
-          break;
-     }
+    case MAINTENANCE:
+      initMaintenance();
+      break;
+
+    default:
+      initFailSafe();
+      break;
+  }
 }
 
 void loop() {
+  switch (currentMode) {
+    case NORMAL:
 
+      return;
+
+    case MAINTENANCE:
+
+      return;
+
+    default:
+
+      return;
+  }
 }
 
 void initNormal() {
-     // docs: https://docs.arduino.cc/tutorials/nano-esp32/esp-now/#code
-     // set as WiFi Station
-     WiFi.mode(WIFI_STA);
+  // docs: https://docs.arduino.cc/tutorials/nano-esp32/esp-now/#code
+  // set as WiFi Station
+  WiFi.mode(WIFI_STA);
 
-     // Init ESP-NOW
-     if (esp_now_init() != ESP_OK) {
-          Serial.println("Error initializing ESP-NOW");
-          return;
-     }
-     // get status of transmitted packet
-     esp_now_register_send_cb(OnDataSent);
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  // get status of transmitted packet
+  esp_now_register_send_cb(OnDataSent);
 
-     // clear out data just in case
-     memset(&peerInfo, 0, sizeof(peerInfo));
+  // clear out data just in case
+  memset(&peerInfo, 0, sizeof(peerInfo));
 
-     // copy the peer address into peer structure
-     memcpy(peerInfo.peer_addr, mainAddress, 6);
+  // copy the peer address into peer structure
+  memcpy(peerInfo.peer_addr, mainAddress, 6);
 
-     peerInfo.channel = 0;  
-     peerInfo.encrypt = false;
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
 
-     // Add peer        
-     if (esp_now_add_peer(&peerInfo) != ESP_OK){
-          Serial.println("Failed to add main board!");
-          return;
-     }
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add main board!");
+    return;
+  }
 }
 
 void initMaintenance() {
-     // docs: https://docs.espressif.com/projects/arduino-esp32/en/latest/api/wifi.html#wi-fi-ap-example
-     // set to AP
-     WiFi.softAP(SSID, PASSWD);
+  // docs: https://docs.espressif.com/projects/arduino-esp32/en/latest/api/wifi.html#wi-fi-ap-example
+  // set to AP
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(SSID, PASSWD);
 }
 
 void initFailSafe() {
-
 }
 
 // callback when data is sent
 void OnDataSent(const wifi_tx_info_t *tx_info, esp_now_send_status_t status) {
-     if (status == ESP_NOW_SEND_SUCCESS) {
-          currentTxState = TX_SUCCESS;
-     } else {
-          currentTxState = TX_FAILED;
-     }
+  if (status == ESP_NOW_SEND_SUCCESS) {
+    currentTxState = TX_SUCCESS;
+  } else {
+    currentTxState = TX_FAILED;
+  }
 }
--- temps/temps.ino
+++ temps/temps.ino
@@ -13,9 +13,8 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
-void setup()
-{
-// start serial port
+void setup() {
+  // start serial port
   Serial.begin(115200);
   delay(1000);
   Serial.println("System Started");
@@ -24,14 +23,13 @@ void setup()
 }
 
 
-void loop()
-{
+void loop() {
   sensors.requestTemperatures();
   // argument 1 = sensor address variable, argument 2 = printed sensor name
   printSensorTemperature(sensor_1, "Sensor#1");
 
   printSensorTemperature(sensor_2, "Sensor#2");
-  delay (5000); // wait 5 seconds before next print
+  delay(5000);  // wait 5 seconds before next print
 }
 
 // function to print sensor temperature
@@ -39,7 +37,7 @@ void printSensorTemperature(DeviceAddress sensor_x, String sensor_name) {
   printAddress(sensor_x);
   Serial.println();
   Serial.print("Temperature " + sensor_name + ": ");
-  Serial.print(sensors.getTempC(sensor_x), 2);    // print with 2 decimals
+  Serial.print(sensors.getTempC(sensor_x), 2);  // print with 2 decimals
   Serial.println("°C");
   Serial.println();
 }
@@ -47,16 +45,16 @@ void printSensorTemperature(DeviceAddress sensor_x, String sensor_name) {
 // function to print sensor address
 void printAddress(DeviceAddress sensor_x) {
   Serial.print("Device address: ");
-    for (uint8_t i = 0; i < 8; i++) {
-      Serial.print("0x");
-      
-      if (sensor_x[i] < 16) {
-        Serial.print("0");
-      }
-      
-      Serial.print(sensor_x[i], HEX);
-      if (i < 7) {
-        Serial.print(", ");
-      }
+  for (uint8_t i = 0; i < 8; i++) {
+    Serial.print("0x");
+
+    if (sensor_x[i] < 16) {
+      Serial.print("0");
     }
+
+    Serial.print(sensor_x[i], HEX);
+    if (i < 7) {
+      Serial.print(", ");
+    }
+  }
 }
