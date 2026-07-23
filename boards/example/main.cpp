8ef67ca276944702e05fb519b1cfc69c38508cd6
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-07-23 15:05:34 +0200

    add an example (that ill have to expand) that ill have to update some day :P

---
+++ boards/example/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * NOTICE:
+ * - You can use this file as a template or starting point for your project.
+ * - Rename `secrets.example.h` to `secrets.h` in `/shared/secrets`.
+ * - Now you can add your credentials/secrets.
+ * - Set BOARD_NAME below to match your hardware setup.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// since im using pioarduino (fork of platformio)
+// you must set this at the top
+#include <Arduino.h>
+
+// you will find some basic configs here
+// also your secrets are imported within this header
+#include <config.h>
+
+void setup() {}
+
+void loop() {}
