2f217b0d6599e9daeab53c7c0b04fdb48ec9b0a1
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-07-24 13:28:40 +0200

    update logger to use character formatting for more flexibility + added new formatting rule
    note that logger.h was ai assisted

--- shared/logs/logger.h
+++ shared/logs/logger.h
@@ -20,55 +20,66 @@ class Log {
         minLevel = level;
     }
 
-    void debug(std::string_view component, std::string_view message) {
+    template <typename... Args>
+    void debug(std::string_view component, const char* fmt, Args&&... args) {
         if (DEBUG < minLevel) {
             return;
         }
-        printTagColor(green, "DEBUG", component, message);
+        printTagColor(green, "DEBUG", component, fmt, std::forward<Args>(args)...);
     }
 
-    void info(std::string_view component, std::string_view message) {
+    template <typename... Args>
+    void info(std::string_view component, const char* fmt, Args&&... args) {
         if (INFO < minLevel) {
             return;
         }
-        printTagColor(blue, "INFO", component, message);
+        printTagColor(blue, "INFO", component, fmt, std::forward<Args>(args)...);
     }
 
-    void warn(std::string_view component, std::string_view message) {
+    template <typename... Args>
+    void warn(std::string_view component, const char* fmt, Args&&... args) {
         if (WARN < minLevel) {
             return;
         }
-        printTagColor(yellow, "WARN", component, message);
+        printTagColor(yellow, "WARN", component, fmt, std::forward<Args>(args)...);
     }
 
-    void err(std::string_view component, std::string_view message) {
+    template <typename... Args>
+    void err(std::string_view component, const char* fmt, Args&&... args) {
         if (ERR < minLevel) {
             return;
         }
-        printFullColor(magenta, "ERROR", component, message);
+        printFullColor(magenta, "ERROR", component, fmt, std::forward<Args>(args)...);
     }
 
-    void crit(std::string_view component, std::string_view message) {
+    template <typename... Args>
+    void crit(std::string_view component, const char* fmt, Args&&... args) {
         if (CRIT < minLevel) {
             return;
         }
-        printFullColor(red, "CRIT", component, message);
+        printFullColor(red, "CRIT", component, fmt, std::forward<Args>(args)...);
     }
 
   private:
     LogLevel minLevel = INFO;
 
+    template <typename... Args>
     void printTagColor(const char* color, const char* level, std::string_view component,
-                       std::string_view message) {
-        Serial.printf("%s[%-5s] [%.*s]%s %.*s\n", color, level, svLen(component), svData(component),
-                      colorReset, svLen(message), svData(message));
+                       const char* fmt, Args&&... args) {
+        Serial.printf("%s[%-5s] [%.*s]%s ", color, level, svLen(component), svData(component),
+                      colorReset);
+
+        argPrintf(fmt, std::forward<Args>(args)...);
+        Serial.printf("\n");
     }
 
     // similar, but prints the whole message colored
+    template <typename... Args>
     void printFullColor(const char* color, const char* level, std::string_view component,
-                        std::string_view message) {
-        Serial.printf("%s[%-5s] [%.*s] %.*s %s\n", color, level, svLen(component),
-                      svData(component), svLen(message), svData(message), colorReset);
+                        const char* fmt, Args&&... args) {
+        Serial.printf("%s[%-5s] [%.*s] ", color, level, svLen(component), svData(component));
+        argPrintf(fmt, std::forward<Args>(args)...);
+        Serial.printf("%s\n", colorReset);
     }
 
     // helpers
@@ -78,6 +89,15 @@ class Log {
 
     const char* svData(std::string_view task) {
         return task.data();
+    }
+
+    template <typename... Args>
+    void argPrintf(const char* fmt, Args&&... args) {
+        if constexpr (sizeof...(Args) == 0) {
+            Serial.printf("%s", fmt);
+        } else {
+            Serial.printf(fmt, std::forward<Args>(args)...);
+        }
     }
 };
 
