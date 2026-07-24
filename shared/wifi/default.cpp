6531b7fd32c173b63d881e66439247a091b48a28
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-07-24 15:40:23 +0200

    added logs to wifi

--- shared/wifi/default.cpp
+++ shared/wifi/default.cpp
@@ -6,28 +6,45 @@
 #include <WiFiType.h>
 
 void WifiManger::setupNormal(const char* ssid, const char* passwd) {
+    logger.debug("wifi", "setting to STA");
+
     WiFi.mode(WIFI_STA);
+
     if (ssid != nullptr && passwd != nullptr) {
         WiFi.begin(ssid, passwd);
+
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(200);
+            logger.info("wifi", "connecting...");
+        }
+        logger.info("wifi", "connected to %s", ssid);
     }
 }
 
 void WifiManger::setupMaint(const char* ssid, const char* passwd) {
+    logger.debug("wifi", "setting to AP");
+
     WiFi.mode(WIFI_AP);
 
     if (passwd == nullptr) {
         passwd = apPasswd;
     }
     WiFi.softAP(ssid, passwd);
+
+    logger.info("wifi", "started AP");
 }
 
 void WifiManger::setupFailsafe(const char* ssid, const char* passwd) {
+    logger.debug("wifi", "setting to AP+STA");
+
     WiFi.mode(WIFI_AP_STA);
 
     if (passwd == nullptr) {
         passwd = apPasswd;
     }
     WiFi.softAP(ssid, passwd);
+
+    logger.info("wifi", "started AP+STA");
 }
 
 WifiManger wifi;
