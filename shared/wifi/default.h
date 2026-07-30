b3ff9878de6ff069cb4e74d8bbada1b848dc9c9f
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-07-30 19:42:58 +0200

    fix: lint

--- shared/wifi/default.h
+++ shared/wifi/default.h
@@ -10,15 +10,14 @@
 inline const char* maintSSID    = TEAM_NAME " - Maintenance " BOARD_NAME;
 inline const char* failsafeSSID = TEAM_NAME " - ERROR " BOARD_NAME;
 
-
 class WifiManger {
   public:
     // sets wifi to WIFI_STA
     void setupNormal(const char* ssid = nullptr, const char* passwd = nullptr);
-    
+
     // sets wifi to WIFI_AP
     void setupMaint(const char* ssid, const char* passwd = nullptr);
-    
+
     // sets wifi to WIFI_AP_STA
     void setupFailsafe(const char* ssid, const char* passwd = nullptr);
 };
