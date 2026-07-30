026c0ad9de6be146c81df79eb1ca46bec03a48bd
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-07-30 14:15:23 +0200

    init

---
+++ shared/OTA/aethr.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class AethrOTA {
+  public:
+  private:
+};
+
+extern AethrOTA aethr;
---
+++ shared/aethrOTA.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// just a wrapper for you
+// for cleaner include path ^^
+#include "./OTA/aethr.h"
