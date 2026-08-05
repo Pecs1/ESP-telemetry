ad0d75f8275156550d9caac2f949b1c7ad0eb7cb
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-08-05 19:24:32 +0200

    chore: update wifi fix: lint

--- shared/wifi/default.cpp
+++ shared/wifi/default.cpp
@@ -1,7 +1,5 @@
 #include "./default.h"
 
-#include "../config.h"
-
 #include <WiFi.h>
 #include <WiFiType.h>
 
@@ -25,10 +23,6 @@ void WifiManger::setupMaint(const char* ssid, const char* passwd) {
     logger.debug("wifi", "setting to AP");
 
     WiFi.mode(WIFI_AP);
-
-    if (passwd == nullptr) {
-        passwd = apPasswd;
-    }
     WiFi.softAP(ssid, passwd);
 
     logger.info("wifi", "started AP");
@@ -38,10 +32,6 @@ void WifiManger::setupFailsafe(const char* ssid, const char* passwd) {
     logger.debug("wifi", "setting to AP+STA");
 
     WiFi.mode(WIFI_AP_STA);
-
-    if (passwd == nullptr) {
-        passwd = apPasswd;
-    }
     WiFi.softAP(ssid, passwd);
 
     logger.info("wifi", "started AP+STA");
--- shared/wifi/default.h
+++ shared/wifi/default.h
@@ -10,16 +10,15 @@
 inline const char* maintSSID    = TEAM_NAME " - Maintenance " BOARD_NAME;
 inline const char* failsafeSSID = TEAM_NAME " - ERROR " BOARD_NAME;
 
-
 class WifiManger {
   public:
     // sets wifi to WIFI_STA
     void setupNormal(const char* ssid = nullptr, const char* passwd = nullptr);
-    
+
     // sets wifi to WIFI_AP
-    void setupMaint(const char* ssid, const char* passwd = nullptr);
-    
+    void setupMaint(const char* ssid, const char* passwd = apPasswd);
+
     // sets wifi to WIFI_AP_STA
-    void setupFailsafe(const char* ssid, const char* passwd = nullptr);
+    void setupFailsafe(const char* ssid, const char* passwd = apPasswd);
 };
 extern WifiManger wifi;
