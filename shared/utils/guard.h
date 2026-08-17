9fe974fab18ee0f6c322f295d63b9b946a0abb2a
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-08-17 19:12:07 +0200

    adjust log severity level

--- shared/utils/guard.h
+++ shared/utils/guard.h
@@ -15,22 +15,22 @@
 
 // stuff behind the scenes
 inline void executeMSG(const char* file, const char* function) {
-    logger.warn(MODULE_NAME, "you can only use \"%s.%s\" once", file, function);
-    logger.info(MODULE_NAME, "skipping \"%s.%s\"...", file, function);
+    logger.err(MODULE_NAME, "you can only use \"%s.%s\" once", file, function);
+    logger.warn(MODULE_NAME, "skipping \"%s.%s\"...", file, function);
 }
 
 inline void executeDepsMSG(const char* calledFile, const char* calledFunction,
                            const char* blockedByFile, const char* blockedByFunction) {
-    logger.warn(MODULE_NAME, "you can use \"%s.%s\" after calling \"%s.%s\"", calledFile,
+    logger.err(MODULE_NAME, "you can use \"%s.%s\" after calling \"%s.%s\"", calledFile,
                 calledFunction, blockedByFile, blockedByFunction);
-    logger.info(MODULE_NAME, "skipping \"%s.%s\"...", calledFile, calledFunction);
+    logger.warn(MODULE_NAME, "skipping \"%s.%s\"...", calledFile, calledFunction);
 }
 
 // similar to "executeDepsMSG", but diff message
 inline void executeBlockMSG(const char* calledFile, const char* calledFunction,
                             const char* blockedByFile, const char* blockedByFunction) {
-    logger.warn(MODULE_NAME, "function \"%s.%s\" is blocked by \"%s.%s\"", calledFile,
+    logger.err(MODULE_NAME, "function \"%s.%s\" is blocked by \"%s.%s\"", calledFile,
                 calledFunction, blockedByFile, blockedByFunction);
-    logger.info(MODULE_NAME, "skipping \"%s.%s\"...", calledFile, calledFunction);
+    logger.warn(MODULE_NAME, "skipping \"%s.%s\"...", calledFile, calledFunction);
 }
 #undef MODULE_NAME
